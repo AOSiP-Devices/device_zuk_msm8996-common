514ae9d5dd3b4cf74f199f4f48fae42e94aa1c4b
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    msm8996-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- msm8996.mk
+++ msm8996.mk
@@ -31,8 +31,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aosip
 
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
